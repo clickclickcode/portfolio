d2f8aa12a612a0a2024d23fdc169021cf141c7ed
clickclickcode <thanatosanyone@gmail.com>
2023-09-22 11:08:17 +0200

    Changed the size of the profile img on mobile.

--- src/components/ImageAndLinks.jsx
+++ src/components/ImageAndLinks.jsx
@@ -4,11 +4,11 @@ import { FaLinkedin } from 'react-icons/fa'
 
 export default function ImageAndLinks() {
     return (
-        <div className='w-32'>
-            <img src={Me} alt="My face" className='w-32 rounded-full grayscale'/>
+        <div className='lg:w-32'>
+            <img src={Me} alt="My face" className='w-44 lg:w-32 rounded-full grayscale'/>
             <div className='flex justify-evenly mt-5 lg:my-5'>
-                <a href="https://www.linkedin.com/in/lean-kenneth-hansen/" target='_blank' className=''><FaLinkedin className='w-6 h-6 mx-auto'/></a>
-                <a href="https://github.com/clickclickcode" target='_blank' className=''><FaGithub className='w-6 h-6 mx-auto'/></a>
+                <a href="https://www.linkedin.com/in/lean-kenneth-hansen/" target='_blank' className=''><FaLinkedin className='w-8 h-8 lg:w-6 lg:h-6 mx-auto'/></a>
+                <a href="https://github.com/clickclickcode" target='_blank' className=''><FaGithub className='w-8 h-8 lg:w-6 lg:h-6 mx-auto'/></a>
             </div>
         </div>
     )
