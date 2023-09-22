83ef4ecb755f8b2bd14347fc3b21a999be92949f
clickclickcode <thanatosanyone@gmail.com>
2023-09-22 10:54:42 +0200

    added some 'lg:' prefixes.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,16 +4,16 @@ import IntroDescription from './IntroDescription'
 
 export default function Intro() {
     return (
-        <div className='flex flex-col gap-5 lg:grid grid-cols-2 grid-rows-2 mx-20 mt-7'>
+        <div className='flex flex-col gap-5 lg:grid lg:grid-cols-2 lg:grid-rows-2 lg:mx-20 lg:mt-7'>
             <div className=''>
                 {/* Container storing name and title */}
                 <NameAndTitle />
             </div>
-            <div className='mx-auto lg:justify-self-end row-span-2'>
+            <div className='mx-auto lg:justify-self-end lg:row-span-2 lg:mx-0'>
                 {/* Container storing image and links */}
                 <ImageAndLinks />
             </div>
-            <div className='text-center'>
+            <div className='text-center lg:text-left'>
                 {/* Short introductory paragraph */}
                 <IntroDescription />
             </div>
