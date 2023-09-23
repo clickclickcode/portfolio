48d90016ab19702280219485917b04b447df4d51
clickclickcode <thanatosanyone@gmail.com>
2023-09-23 16:35:02 +0200

    Credited the author of a helpful tutorial.

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 
+/* This menu was created with the aid of the following tutorial:
+    LeadEngineer - React Hamburger Menu Tutorial
+    https://youtu.be/-aNCsBrnn8k?si=-gQl6FinzosJ2cP8
+*/
+
 export default function HamburgerMenu() {
     
     const[menuOpen, setMenuOpen] = useState(false)
