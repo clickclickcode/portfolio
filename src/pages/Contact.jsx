d824a74306a6d5d0af05c0af0853b52de38fa445
clickclickcode <thanatosanyone@gmail.com>
2023-09-27 17:04:50 +0200

    Updates, content and structure to pages.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,39 @@
 export default function Contact() {
+    
+    const contacts = [
+        {
+            id: 1,
+            title: 'Email',
+            url: '',
+            icon: '',
+            description: 'Email description',
+        },
+        {
+            id: 2,
+            title: 'Github',
+            url: '',
+            icon: '',
+            description: 'Github description',
+        },
+        {
+            id: 3,
+            title: 'LinkedIn',
+            url: '',
+            icon: '',
+            description: 'LinkedIn description',
+        }
+    ];
+    
     return (
         <div>
             <h1>Contact</h1>
-            <p>Contact page</p>
+            <p>Should any of this have caught your eye, you are more than welcome to reach out, be it for a friendly chat, potential collaborations or anything inbetween. You can catch me via the contacts below.</p>
+            {contacts.map(contact => (
+                <div key={contact.id} className="">
+                    <h2>{contact.title}</h2>
+                    <p>{contact.description}</p>
+                </div>
+            ))}
         </div>
     )
 }
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -18,14 +18,40 @@ const projects = [
     },
 ];
 
+const tools = [
+    {
+        id: 1,
+        img: '',
+        alt: '',
+    },
+    {
+        id: 2,
+        img: '',
+        alt: '',
+    },
+    {
+        id: 3,
+        img: '',
+        alt: '',
+    },
+];
+
 export default function Work() {
     return (
         <div className=''>
             <div className='flex flex-col gap-5 my-5 mx-20'>
                 <h1 className='text-2xl'>Personal & School Projects</h1>
-                <p className='text-md'>Here You can have a look around to see what projects I have been working on up until this moment. Hopefully, this will give you an idea of who I am as a developer. Happy browsing!</p>
+                <p className='text-md'>Here you can have a look around to see what projects I have been working on up until this moment. Hopefully, this will give you an idea of who I am as a developer. Happy browsing!</p>
             </div>
             <div>
+                <h2 className='text-2xl my-5 mx-20'>Tools</h2>
+                <div className='flex overflow-scroll'>
+                    {tools.map(tool => (
+                        <div key={tool.id} className='flex flex-col gap-5 mx-20 my-10'>
+                            <img src={`../images/${tool.img}`} alt={tool.alt} />
+                        </div>
+                    ))}
+                </div>
                 <h2 className='text-2xl my-5 mx-20'>School Projects</h2>
                 <div className='flex overflow-scroll'>
                     {projects.map(project => (
