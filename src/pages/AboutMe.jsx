11dca11af9542cb4fe8f008958b440850d920fb2
clickclickcode <thanatosanyone@gmail.com>
2023-09-27 16:50:45 +0200

    Added some structure to the 'about me' page.

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,8 +1,33 @@
 export default function AboutMe() {
+    
+    const hobbies = [
+        {
+            id: 1,
+            title: 'Coding',
+            description: 'Coding description',
+        },
+        {
+            id: 2,
+            title: 'Bouldering',
+            description: 'Bouldering description',
+        },
+        {
+            id: 3,
+            title: 'Gaming',
+            description: 'Gaming description',
+        },
+    ];
+    
     return (
-        <div>
+        <div className="flex flex-col items-center gap-5 my-5">
             <h1>About Me</h1>
-            <p>About Me page</p>
+            <h2>Get to know me a bit better</h2>
+            {hobbies.map(hobby => (
+                <div key={hobby.id} className="">
+                    <h2>{hobby.title}</h2>
+                    <p>{hobby.description}</p>
+                </div>
+            ))}
         </div>
     )
 }
