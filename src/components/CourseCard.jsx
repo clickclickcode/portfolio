abd6d4335253f4fba78143daa87ed52ae0edc116
clickclickcode <thanatosanyone@gmail.com>
2023-11-21 13:30:31 +0100

    object-contain and better images for platforms

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -5,7 +5,7 @@ export default function CourseCard({ course }) {
 
     return (
         <div className='w-52 h-[400px] flex flex-col border rounded-md'>
-            <img src={course.img} alt="" className="h-40 w-52 object-cover rounded-t-md grayscale md:hover:grayscale-0" />
+            <img src={course.img} alt="" className="h-40 w-52 object-contain rounded-t-md grayscale md:hover:grayscale-0" />
             <div className="flex flex-col justify-between flex-1 h-[200px] w-52 border-t px-1 py-2">
                 <div className='flex flex-col gap-4'>
                     <div className='flex justify-between'>
