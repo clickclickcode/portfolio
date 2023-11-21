80237a55e966c722b45f58c49895ac49e9d0e963
clickclickcode <thanatosanyone@gmail.com>
2023-11-21 20:13:31 +0100

    adjusting icon size + centering hobbies

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,7 +8,7 @@ export default function AboutMe() {
         <div className="flex flex-col gap-5 mx-12 pt-14" id='about'>
             <h2 className="text-2xl mb-1">About Me</h2>
             <p className="text-[15px]">As a person I am down-to-earth and open-minded. I appreciate opportunities to learn from people smarter than myself.<br/><br/>Prior to starting this journey to become a developer, I studied Marketing Management where I took a liking to SEO.<br/>I would not be against updating what little SEO knowledge that persisted since then.<br/><br/>In my spare time some of the things I enjoy the most are:</p>
-            <div className="flex items-center gap-6 py-4 px-2 overflow-scroll">
+            <div className="flex items-center gap-6 py-4 px-2 overflow-scroll md:justify-center">
                 {hobbies.map(hobby => (
                     <HobbyCard key={hobby.id} hobby={hobby} />
                 ))}
--- src/components/ImageAndLinks.jsx
+++ src/components/ImageAndLinks.jsx
@@ -8,8 +8,8 @@ export default function ImageAndLinks() {
             {/* md:w-32 */}
             <img src='/images/me.png' alt="My face" className='w-44 md:w-44 rounded-full grayscale'/>
             <div className='flex justify-evenly mt-5 md:my-5'>
-                <a href="https://www.linkedin.com/in/lean-kenneth-hansen/" target='_blank' className=''><FaLinkedin className='w-10 h-10 md:w-6 md:h-6 mx-auto'/></a>
-                <a href="https://github.com/clickclickcode" target='_blank' className=''><FaGithub className='w-10 h-10 md:w-6 md:h-6 mx-auto'/></a>
+                <a href="https://www.linkedin.com/in/lean-kenneth-hansen/" target='_blank' className=''><FaLinkedin className='w-10 h-10 mx-auto'/></a>
+                <a href="https://github.com/clickclickcode" target='_blank' className=''><FaGithub className='w-10 h-10 mx-auto'/></a>
             </div>
         </div>
     )
